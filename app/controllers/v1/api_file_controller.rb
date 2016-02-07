6ac6a156127cbac48e02515cc67aa726e74ca972
Javier Bravo <javibravo85@gmail.com>
2016-02-07 22:29:40 +0000

    Add TextFile model and save data in request

--- app/controllers/v1/api_file_controller.rb
+++ app/controllers/v1/api_file_controller.rb
@@ -5,9 +5,11 @@ module V1
       if params[:file].size > RubyChallenge::Application.config.file_max_size
         render :status => :request_header_fields_too_large, :text => "File size exceeded, max 10MB"
       else
-        file_name = SecureRandom.uuid()
-        path = File.join(Rails.root, RubyChallenge::Application.config.file_store_path, file_name)
+        text_file = TextFile.new
+        text_file.name = SecureRandom.uuid()
+        text_file.original_name = params[:file].original_filename
 
+        path = File.join(Rails.root, RubyChallenge::Application.config.file_store_path, text_file.name)
         uploaded_file = File.open(path, "w+")
         uploaded_file.write(params[:file].read)
         uploaded_file.rewind
@@ -15,8 +17,13 @@ module V1
         word_counter = WordsCounter.new(uploaded_file)
         word_counter.parse
 
+        text_file.total_words = word_counter.total
+        text_file.distinct_words = word_counter.distinct
+        text_file.words = word_counter.words.to_json
+        text_file.save
+
         response = {
-            'name' => file_name,
+            'name' => text_file.name,
             'total' => word_counter.total,
             'distinct' => word_counter.distinct,
             'words' => word_counter.words
---
+++ db/migrate/20160207222220_create_text_files.rb
@@ -0,0 +1,15 @@
+class CreateTextFiles < ActiveRecord::Migration
+  def change
+    create_table :text_files do |t|
+      t.string :name
+      t.string :original_name
+      t.integer :total_words
+      t.integer :distinct_words
+      t.text :words
+      t.datetime :created_at
+
+      t.timestamps null: false
+    end
+    add_index :text_files, :name
+  end
+end
