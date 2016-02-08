f1332faa7a1bdd29920fe5a6568b0edf081f2ef7
Javier Bravo <javibravo85@gmail.com>
2016-02-08 21:03:37 +0000

    Add unique validator for TextFile.name

--- app/models/text_file.rb
+++ app/models/text_file.rb
@@ -1,4 +1,7 @@
 class TextFile < ActiveRecord::Base
+
+  validates :name, uniqueness: true
+
   def getPath
     File.join(Rails.root, RubyChallenge::Application.config.file_store_path, self.name)
   end
